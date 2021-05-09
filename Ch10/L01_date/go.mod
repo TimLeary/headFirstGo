117e0ba5daea5e14d418635960e35cb6247bda8c
TimLeary <dnasztanovics@gmail.com>
2021-05-09 17:49:51 +0200

    use getters

--- Ch10/L01_date/go.mod
+++ Ch10/L01_date/go.mod
@@ -2,4 +2,4 @@ module github.com/TimLeary/headFirstGo/Ch10/L01_date
 
 go 1.16
 
-require github.com/TimLeary/headFirstGo v0.0.0-20210506124048-360ade22a4c5 // indirect
+require github.com/TimLeary/headFirstGo v0.0.0-20210509113158-c7974e4f0283
--- Ch10/L01_date/main.go
+++ Ch10/L01_date/main.go
@@ -1,24 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"github.com/TimLeary/headFirstGo/calendar"
+	"log"
+)
+
 func main()  {
-	/*
-	date := Date{}
+	date := calendar.Date{}
 	err := date.SetYear(2019)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(date.Year)
+	fmt.Println(date.Year())
 
 	err = date.SetMonth(5)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(date.Month)
+	fmt.Println(date.Month())
 
 	err = date.SetDay(27)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(date.Day)
-	 */
+	fmt.Println(date.Day())
 }
