c7974e4f0283d6e690b3c5d35ef350284109338c
TimLeary <dnasztanovics@gmail.com>
2021-05-09 13:31:58 +0200

    add calendar getters

---
+++ Ch10/L01_date/go.mod
@@ -0,0 +1,5 @@
+module github.com/TimLeary/headFirstGo/Ch10/L01_date
+
+go 1.16
+
+require github.com/TimLeary/headFirstGo v0.0.0-20210506124048-360ade22a4c5 // indirect
--- calendar/date.go
+++ calendar/date.go
@@ -16,6 +16,10 @@ func (d *Date) SetYear(year int) error {
 	return nil
 }
 
+func (d *Date) Year() int {
+	return d.year
+}
+
 func (d *Date) SetMonth(month int) error {
 	if month < 1 || month > 12 {
 		return errors.New("invalid month")
@@ -24,10 +28,18 @@ func (d *Date) SetMonth(month int) error {
 	return nil
 }
 
+func (d *Date) Month() int {
+	return d.month
+}
+
 func (d *Date) SetDay(day int) error {
 	if day < 1 || day > 31 {
 		return errors.New("invalid day")
 	}
 	d.day = day
 	return nil
+}
+
+func (d *Date) Day() int {
+	return d.day
 }
