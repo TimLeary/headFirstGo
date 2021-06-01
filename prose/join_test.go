75fea87da96a01aa47c87ac98e1630011ea463fc
TimLeary <dnasztanovics@gmail.com>
2021-06-01 11:44:29 +0200

    Table-driven tests

--- prose/join_test.go
+++ prose/join_test.go
@@ -35,3 +35,24 @@ func TestThreeElements(t *testing.T)  {
 func errorString(list []string, got string, want string) string {
 	return fmt.Sprintf("JoinWithCommas(%#v) = \"%s\", want \"%s\"", list, got, want)
 }
+
+type testData struct {
+	list []string
+	want string
+}
+
+func TestJoinWithCommas(t *testing.T) {
+	tests := []testData {
+		{list: []string{}, want: ""},
+		testData{list: []string{"apple"}, want: "apple"},
+		testData{list: []string{"apple", "orange"}, want: "apple and orange"},
+		testData{list: []string{"apple", "orange", "pear"}, want: "apple, orange, and pear"},
+	}
+
+	for _, test := range tests {
+		got := JoinWithCommas(test.list)
+		if got != test.want {
+			t.Errorf("JoinWithCommas(%#v) = \"%s\", want \"%s\"", test.list, got, test.want)
+		}
+	}
+}
